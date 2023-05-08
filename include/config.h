c83032aecece458b8cfa69107e0a19c0396dcca3
Friedel Schön <derfriedmundschoen@gmail.com>
2023-05-08 09:39:04 +0200

    set config-constants if not defined

--- include/config.h
+++ include/config.h
@@ -1,47 +1,116 @@
 #pragma once
 
 // environment variable where the current runlevel is stored
-#define SV_RUNLEVEL_ENV "SERVICE_RUNLEVEL"
+#ifndef SV_RUNLEVEL_ENV
+#	define SV_RUNLEVEL_ENV "SERVICE_RUNLEVEL"
+#endif
+
 // seconds to wait for a service before it gets killed
-#define SV_STOP_TIMEOUT 5
+#ifndef SV_STOP_TIMEOUT
+#	define SV_STOP_TIMEOUT 5
+#endif
+
 // maximal characters a service-dir can have
-#define SV_NAME_MAX 512
+#ifndef SV_NAME_MAX
+#	define SV_NAME_MAX 512
+#endif
+
 // maximal dependencies a service can have
-#define SV_DEPENDS_MAX 16
+#ifndef SV_DEPENDS_MAX
+#	define SV_DEPENDS_MAX 16
+#endif
+
 // maximal amount a service may fail
-#define SV_FAIL_MAX 32
+#ifndef SV_FAIL_MAX
+#	define SV_FAIL_MAX 32
+#endif
+
 // maximal amount of services that can be registered
-#define SV_SERVICE_MAX 128
+#ifndef SV_SERVICE_MAX
+#	define SV_SERVICE_MAX 128
+#endif
+
 // default runlevel
-#define SV_RUNLEVEL "default"
+#ifndef SV_RUNLEVEL
+#	define SV_RUNLEVEL "default"
+#endif
+
 // path to service-dir
-#define SV_SERVICE_DIR "/etc/service.d"
+#ifndef SV_SERVICE_DIR
+#	define SV_SERVICE_DIR "/etc/service.d"
+#endif
+
 // path to start-script
-#define SV_START_EXEC "/usr/share/fiss/start"
+#ifndef SV_START_EXEC
+#	define SV_START_EXEC "/usr/share/fiss/start"
+#endif
+
 // path to stop-script
-#define SV_STOP_EXEC "/usr/share/fiss/stop"
+#ifndef SV_STOP_EXEC
+#	define SV_STOP_EXEC "/usr/share/fiss/stop"
+#endif
+
 // path to suspend-script
-#define SV_SUSPEND_EXEC "/usr/share/fiss/suspend"
+#ifndef SV_SUSPEND_EXEC
+#	define SV_SUSPEND_EXEC "/usr/share/fiss/suspend"
+#endif
+
 // path to resume-script
-#define SV_RESUME_EXEC "/usr/share/fiss/resume"
+#ifndef SV_RESUME_EXEC
+#	define SV_RESUME_EXEC "/usr/share/fiss/resume"
+#endif
+
 // the current version
-#define SV_VERSION "0.1.0"
+#ifndef SV_VERSION
+#	define SV_VERSION "0.1.0"
+#endif
+
 // time to wait to accept new connection
-#define SV_ACCEPT_INTERVAL 1    // seconds
+#ifndef SV_ACCEPT_INTERVAL
+#	define SV_ACCEPT_INTERVAL 1    // seconds
+#endif
+
 // control socket (%s is the runlevel)
-#define SV_CONTROL_SOCKET "/run/fiss/control-%s.socket"
+#ifndef SV_CONTROL_SOCKET
+#	define SV_CONTROL_SOCKET "/run/fiss/control-%s.socket"
+#endif
+
 // maximal size of <service>/params
-#define SV_PARAM_FILE_MAX 16384    // 16kb
+#ifndef SV_PARAM_FILE_MAX
+#	define SV_PARAM_FILE_MAX 16384    // 16kb
+#endif
 
-#define SV_PARAM_FILE_LINE_MAX 1024    // 16kb
+// maximal size of a param in ./params
+#ifndef SV_PARAM_FILE_LINE_MAX
+#	define SV_PARAM_FILE_LINE_MAX 1024    // 16kb
+#endif
 
-#define SV_ENV_FILE_LINE_MAX 1024    // 16kb
+// maximal size of a line in ./env
+#ifndef SV_ENV_FILE_LINE_MAX
+#	define SV_ENV_FILE_LINE_MAX 1024    // 16kb
+#endif
+
 // shell to enter if fiss failed
-#define SV_RESCUE_SHELL "/bin/bash"
-// max dependencies in the dep-tree
-#define SV_DEPENDENCY_MAX 512
-// max arguments a service can have
-#define SV_ARGUMENTS_MAX 16
-#define SV_ENV_MAX       16
+#ifndef SV_RESCUE_SHELL
+#	define SV_RESCUE_SHELL "/bin/bash"
+#endif
 
-#define SV_LOG_DIR "/var/log/fiss"
+// max dependencies in the dep-tree
+#ifndef SV_DEPENDENCY_MAX
+#	define SV_DEPENDENCY_MAX 512
+#endif
+
+// max arguments a service can have
+#ifndef SV_ARGUMENTS_MAX
+#	define SV_ARGUMENTS_MAX 16
+#endif
+
+// maximal count of environment variables in ./env
+#ifndef SV_ENV_MAX
+#	define SV_ENV_MAX 16
+#endif
+
+// defines the directory where logs are stored
+#ifndef SV_LOG_DIR
+#	define SV_LOG_DIR "/var/log/fiss"
+#endif
